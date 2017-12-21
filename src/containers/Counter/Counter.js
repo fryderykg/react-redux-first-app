853c0dcfe23bcdd9afeddfc205f789bc744b4b49
FryderykG <eszkimos@gmail.com>
2017-12-21 21:40:07 +0100

    add utility function to update objects in reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,58 +4,57 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import * as actionCreators from '../../store/actions/index';
 
-
 import './Counter.css';
 
 class Counter extends Component {
 
-    render() {
-        const resultsArr = this.props.results.map(result => {
-            return (
-                <li className="CounterResult"
-                    onClick={() => this.props.onDeleteResult(result.id)}
-                    key={result.id}>
-                    {result.value}
-                </li>
-            )
-        });
+  render() {
+    const resultsArr = this.props.results.map(result => {
+      return (
+        <li className="CounterResult"
+            onClick={() => this.props.onDeleteResult(result.id)}
+            key={result.id}>
+          {result.value}
+        </li>
+      )
+    });
 
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr}/>
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}/>
-                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}/>
-                <hr/>
-                <button className="CounterStoreBtn"
-                        onClick={() => this.props.onStoreResult(this.props.ctr)}>
-                    Store Result
-                </button>
-                <ul className="CounterResultsList">
-                    {resultsArr}
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr}/>
+        <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
+        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
+        <CounterControl label="Add 10" clicked={this.props.onAddCounter}/>
+        <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}/>
+        <hr/>
+        <button className="CounterStoreBtn"
+                onClick={() => this.props.onStoreResult(this.props.ctr)}>
+          Store Result
+        </button>
+        <ul className="CounterResultsList">
+          {resultsArr}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        ctr: state.ctr.counter,
-        results: state.res.results
-    }
+  return {
+    ctr: state.ctr.counter,
+    results: state.res.results
+  }
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch(actionCreators.increment()),
-        onDecrementCounter: () => dispatch(actionCreators.decrement()),
-        onAddCounter: () => dispatch(actionCreators.add(10)),
-        onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
-        onStoreResult: (value) => dispatch(actionCreators.storeResult(value)),
-        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
-    }
+  return {
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(10)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+    onStoreResult: (value) => dispatch(actionCreators.storeResult(value)),
+    onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,31 +1,28 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from "../utility";
 
 const initialState = {
-    results: []
+  results: []
 };
 
-const reducer = (state=initialState, action) => {
-    switch (action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
 
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({
-                    id: Date.now(),
-                    value: action.value
-                })
-            };
-        case actionTypes.DELETE_RESULT:
-            const updatedResults = state.results.filter(result => {
-                return result.id !== action.id
-            });
-            return {
-                ...state,
-                results: updatedResults
-            };
-        default:
-            return state
-    }
+    case actionTypes.STORE_RESULT:
+      return updateObject(state, {
+        results: state.results.concat({
+          id: Date.now(),
+          value: action.value
+        })
+      });
+    case actionTypes.DELETE_RESULT:
+      const updatedResults = state.results.filter(result => {
+        return result.id !== action.id
+      });
+      return updateObject(state, {results: updatedResults});
+    default:
+      return state
+  }
 };
 
 export default reducer
